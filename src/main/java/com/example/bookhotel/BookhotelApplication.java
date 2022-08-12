b1250a61c45f9a68e88eebf3a61efcd2c76c0d91
ggeorg1eva <ggeorgieva1703@gmail.com>
2022-08-12 18:25:24 +0300

    Last changes

--- src/main/java/com/example/bookhotel/BookhotelApplication.java
+++ src/main/java/com/example/bookhotel/BookhotelApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BookhotelApplication {
-
+//Gergana Georgieva softuni java web 2022
     public static void main(String[] args) {
         SpringApplication.run(BookhotelApplication.class, args);
     }
